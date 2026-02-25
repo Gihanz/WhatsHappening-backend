e02cf7ea8e0ede1718c5b749714dd6eceaabce6d
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2026-02-25 04:41:52 -0500

    Update events.js

--- api/cron/events.js
+++ api/cron/events.js
@@ -1,4 +1,4 @@
-// Vercel Serverless Function - Weekly Events Post
+fffff// Vercel Serverless Function - Weekly Events Post
 // This function is triggered by GitHub Actions
 
 const { getEvents } = require('../../scrapers/events');
